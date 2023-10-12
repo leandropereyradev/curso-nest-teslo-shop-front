d48526a29e07523076b0731c8e4079d924b3856d
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-10-12 17:07:48 +0200

    JWToken

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <h1>Websocket Client</h1>
 
+    <div>
+      <input id='jw-token' placeholder='JSON Web Token'/>
+      <button id='btn-connect'>Connect</button>
+    </div>
+
+    <br>
+
     <span id='server-status'>offline</span>
 
     <ul id='clients-ul'></ul>
@@ -18,4 +25,13 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   </div>
 `;
 
-connectToServer();
+// connectToServer();
+
+const jwToken = document.querySelector<HTMLInputElement>("#jw-token")!;
+const btnConnect = document.querySelector<HTMLButtonElement>("#btn-connect")!;
+
+btnConnect.addEventListener("click", () => {
+  if (jwToken.value.trim().length <= 0) return alert("Enter a valid JWToken");
+
+  connectToServer(jwToken.value.trim());
+});
--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,8 +1,12 @@
 import { Manager } from "socket.io-client";
 import { Socket } from "socket.io-client";
 
-export const connectToServer = () => {
-  const manager = new Manager("http://localhost:3000/socket.io/socket.io.js");
+export const connectToServer = (token: string) => {
+  const manager = new Manager("http://localhost:3000/socket.io/socket.io.js", {
+    extraHeaders: {
+      authentication: token,
+    },
+  });
 
   const socket = manager.socket("/");
 
