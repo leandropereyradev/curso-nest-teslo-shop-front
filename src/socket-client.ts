44c754fb6b9244c99f43c14cde63fecfb6835c28
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-10-12 12:01:59 +0200

    Detect connection and disconnection

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,7 +1,22 @@
 import { Manager } from "socket.io-client";
+import { Socket } from "socket.io-client";
 
 export const connectToServer = () => {
   const manager = new Manager("http://localhost:3000/socket.io/socket.io.js");
 
   const socket = manager.socket("/");
+
+  addListeners(socket);
+};
+
+const addListeners = (socket: Socket) => {
+  const serverStatusLabel = document.querySelector("#server-status")!;
+
+  socket.on("connect", () => {
+    serverStatusLabel.innerHTML = "connected";
+  });
+
+  socket.on("disconnect", () => {
+    serverStatusLabel.innerHTML = "disconnected";
+  });
 };
